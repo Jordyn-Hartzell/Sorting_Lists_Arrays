658350134ca076b570581aac10a58565a8b7e422
Jordyn Hartzell <hartzellj@student.swosu.edu>
2015-11-17 13:12:22 -0600

    Added menu options with switch statement

--- src/Sorting_Numbers_Array.java
+++ src/Sorting_Numbers_Array.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Sorting_Numbers_Array {
 	public static void main(String[] args) {
 		
@@ -7,6 +8,16 @@ public class Sorting_Numbers_Array {
 		menuOption();
 	}
 	public static void menuOption(){
+		
 		System.out.println("If this runs, menuOption works");
+		
+		System.out.println("Choose 1 or 2");
+		Scanner input = new Scanner(System.in);
+		int userChoice = input.nextInt();
+		
+		switch(userChoice){
+		case 1: System.out.println("1"); break;
+		case 2: System.out.println("2");
+		}
 	}
 }
